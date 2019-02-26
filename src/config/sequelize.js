d094628ef94b5c608ccfbbc28af156d06eff15a8
Verma <avisverma@deloitte.com>
2019-02-26 17:08:19 +0530

    updated sequelize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,24 +1,8 @@
 module.exports = {
   development: {
     username: 'root',
-    password: 'root@123',
+    password: '',
     database: 'fest_dev',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    define: { timestamps: false }
-  },
-  test: {
-    username: 'root',
-    password: 'root@123',
-    database: 'fest_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    define: { timestamps: false }
-  },
-  production: {
-    username: 'root',
-    password: 'root@123',
-    database: 'fest_prod',
     host: '127.0.0.1',
     dialect: 'mysql',
     define: { timestamps: false }
