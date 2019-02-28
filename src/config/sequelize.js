284234c831a6bd4d03c37638cbb04807fbb9d0be
Verma <avisverma@deloitte.com>
2019-02-28 14:53:07 +0530

    change config file

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: 'avishkar',
-    password: '',
+    password: 'test123456',
     database: 'fest_dev',
     host: '127.0.0.1',
     dialect: 'mysql',
