70bc1af4b82eb467d29cd978f53ad134b7227c19
Verma <avisverma@deloitte.com>
2019-02-28 15:53:28 +0530

    config

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,4 @@ var _server = _interopRequireDefault(require("./server"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_server.default.init(4567);
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-server.listen(server_port, server_ip_address, function () {
-  console.log("Listening on " + server_ip_address + ", port " + server_port);
-});
+_server.default.init();
