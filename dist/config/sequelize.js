9200165630287022b240d9296bebc37ff683ef47
Verma <avisverma@deloitte.com>
2019-02-28 15:38:03 +0530

    dist file

---
+++ dist/config/sequelize.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+  development: {
+    username: 'avishkar',
+    password: 'test123456',
+    database: 'fest_dev',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    define: {
+      timestamps: false
+    }
+  }
+};
---
+++ dist/server/routes/product.route.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+var _product = _interopRequireDefault(require("../controllers/product.controller"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var productRoute = _express.default.Router();
+
+productRoute.get('/:id', _product.default.getProduct);
+productRoute.get('/', _product.default.getProducts);
+productRoute.get('/description/:id', _product.default.getProductDescription);
+var _default = productRoute;
+exports.default = _default;
